6746db7a0413fa5116d4be7700bf55ca96cfd852
Sakii470 <70532979+Sakii470@users.noreply.github.com>
2021-03-16 16:15:34 +0100

    Add files via upload

---
+++ rozdział 04/ćwicz02.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    string dessert;
+
+    cout << "Podaj swoje imię: \n";
+    getline(cin, name);              //przy pomocy funkcji getline(strumien, nazwa zmiennej) wczytałem cały łancuch znakowy skladajacy sie z wielu wyrazow az do napotkania znaku '\\n'- klawisz enter
+    cout << "Podaj swoj ulubiony deser: \n";
+    getline(cin, dessert);
+
+    cout << "Mam dla Ciebie " << dessert << ", " << name << endl;
+    
+}
+
